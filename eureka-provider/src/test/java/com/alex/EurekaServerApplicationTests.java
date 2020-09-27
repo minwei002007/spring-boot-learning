cb984f79e46aa0462160b957364799c368070088
闵巍 <minwei@rainbowcn.com>
2020-09-27 15:39:37 +0800

    springbootTest  整合

--- eureka-provider/src/test/java/com/alex/EurekaServerApplicationTests.java
+++ eureka-provider/src/test/java/com/alex/EurekaServerApplicationTests.java
@@ -1,13 +1,17 @@
 package com.alex;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
 class EurekaServerApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		System.out.println("1111");
 	}
 
 }
